7e0deb95139e7d232a8663181fe27cb93c45fd80
alexlivj <alexlivjones@gmail.com>
2025-03-03 12:43:22 -0700

    Added screens
    Created empty screens for the title scene and core game

---
+++ core/src/main/java/simulator/elevator/screen/GameScreen.java
@@ -0,0 +1,49 @@
+package simulator.elevator.screen;
+
+import com.badlogic.gdx.Screen;
+
+import simulator.elevator.Main;
+
+public class GameScreen implements Screen {
+    private Main game;
+    
+    public GameScreen(Main game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void render(float delta) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+    }
+    
+}
